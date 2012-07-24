d51d61363e7d142193899114eafb7e66f06273e6
B Kleinen <kleinen@infrastructure.de>
2012-07-24 16:34:26 +0200

    cleaned up code according to metrics

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TutorialSampleApp32FromStart::Application.config.secret_token = 'e50ea30717a00a8f0af69833f7015b499bf0a1643b15ef13990650c40a355eef7d260a360e39bbc28ed78e84a0d9110c64ad9714ef8723565a9eec168edb04c6'
+TutorialSampleApp32FromStart::Application.config.secret_token =
+'e50ea30717a00a8f0af69833f7015b499bf0a1643b15ef13990650c40a355eef7d260a360e39bbc28ed78e84a0d9110c64ad9714ef8723565a9eec168edb04c6'
