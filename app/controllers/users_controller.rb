97f2ff4cfdde94f8c7f83cb3f770a5e50c667543
B Kleinen <kleinen@infrastructure.de>
2012-07-26 00:10:09 +0200

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   def show
     @user = User.find(params[:id])
+  end
+  def create
+    @user = User.create(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
