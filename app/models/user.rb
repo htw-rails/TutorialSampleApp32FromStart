17db9e4339d63de8fc0518c6827167edb6e9be70
B Kleinen <kleinen@infrastructure.de>
2012-07-25 16:57:59 +0200

    added index for user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false}
+  before_save { |user| user.email = email.downcase }
 end
