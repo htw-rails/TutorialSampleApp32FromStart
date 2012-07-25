6ce088fe4b7b75a2027f8e571f2f0c188891af66
B Kleinen <kleinen@infrastructure.de>
2012-07-25 15:49:56 +0200

    used ApplicationHelper method in test

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,14 +1,5 @@
 include ApplicationHelper
 
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
-
 def valid_signin(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
