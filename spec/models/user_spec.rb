e39cb927afe96e2afb6222026d7079c5584a054f
B Kleinen <kleinen@infrastructure.de>
2012-07-25 17:24:33 +0200

    exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -80,6 +80,15 @@ describe User do
       it { should_not be_valid }
     end
   end
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
