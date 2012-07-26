a4397ce132f7420a20f49eff36cd07ebb0ea391b
B Kleinen <kleinen@infrastructure.de>
2012-07-26 17:03:03 +0200

    user edit form completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def edit
+    @user = User.find(params[:id]) 
+  end
   def show
     @user = User.find(params[:id])
   end
