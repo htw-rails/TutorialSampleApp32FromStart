4f2e8fa877733ccb8a92e958528d9e3c2c55ecab
B Kleinen <kleinen@infrastructure.de>
2012-07-27 00:23:34 +0200

    only admins can delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -38,7 +39,9 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 private
-
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
   def signed_in_user
     unless signed_in?
       store_location
