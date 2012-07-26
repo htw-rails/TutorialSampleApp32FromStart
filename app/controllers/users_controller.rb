ca34f3cc030b01b1183afdbd85f822fe09c561a0
B Kleinen <kleinen@infrastructure.de>
2012-07-26 19:39:01 +0200

    authorisation: right user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
   end
   def edit
-    @user = User.find(params[:id]) 
   end
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -29,9 +28,13 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  private
+private
 
-    def signed_in_user
-      redirect_to signin_path, notice: "Please sign in." unless signed_in?
-    end
+  def signed_in_user
+    redirect_to signin_path, notice: "Please sign in." unless signed_in?
+  end
+  def correct_user
+     @user = User.find(params[:id])
+     redirect_to(root_path) unless current_user?(@user)
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,4 +17,7 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+  def current_user?(user)
+    user == current_user
+  end
 end
