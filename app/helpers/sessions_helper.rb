0eeda7ccadb140eff27aa9a88cf13b6583cf252c
B Kleinen <kleinen@infrastructure.de>
2012-07-26 19:48:08 +0200

    friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,4 +20,13 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -65,6 +65,25 @@ describe "Authentication" do
         specify { response.should redirect_to(root_path) }
       end
     end
+    describe "friendly forwarding for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end
+      end
+    end
     
   end
 end
