b0ea3d39cb6a4577a5b940e53c351f48d1c67a24
B Kleinen <kleinen@infrastructure.de>
2012-07-28 01:59:37 +0200

    10.3.1 Access Controll for micropost actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,12 +49,6 @@ private
     @user = User.find(params[:id])
     redirect_to(root_path) if current_user == @user
   end
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_path, notice: "Please sign in." 
-    end
-  end
   def correct_user
      @user = User.find(params[:id])
      redirect_to(root_path) unless current_user?(@user)
