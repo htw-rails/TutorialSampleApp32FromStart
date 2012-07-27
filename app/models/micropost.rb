8f7e6e3d94c39d784ba8eeb686200a968541ffd9
B Kleinen <kleinen@infrastructure.de>
2012-07-27 13:50:12 +0200

    10.1.3 microposts

---
+++ app/models/micropost.rb
@@ -0,0 +1,5 @@
+class Micropost < ActiveRecord::Base
+  attr_accessible :content
+  belongs_to :user
+  validates :user_id, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
  
+  has_many :microposts
+   
   before_save { self.email.downcase! }
   before_save :create_remember_token
   validates :name, presence: true, length: {maximum: 50}
