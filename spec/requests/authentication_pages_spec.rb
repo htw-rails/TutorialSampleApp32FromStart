f33b7b80971d28a96646d9df0749b1d284310fee
B Kleinen <kleinen@infrastructure.de>
2012-07-27 12:03:40 +0200

    9.6.7 test for deleting stored page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -115,6 +115,13 @@ describe "Authentication" do
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
+          
+          describe "when signing in again" do
+            before { sign_in(user) }
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name )
+            end
+          end
         end
       end
       describe "as non-admin user" do
