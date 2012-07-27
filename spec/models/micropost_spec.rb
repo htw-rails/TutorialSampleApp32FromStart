43e147d846d24de9a0225fb8909ece87600e60fe
B Kleinen <kleinen@infrastructure.de>
2012-07-27 14:26:30 +0200

    10.1.5 micropost validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -18,10 +18,24 @@ describe Micropost do
     it { should_not be_valid }
   end
   describe "accessible attributes" do
-      it "should not allow access to user_id" do
-        expect do
-          Micropost.new(user_id: user.id)
-        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
-      end    
-    end
+    it "should not allow access to user_id" do
+      expect do
+        Micropost.new(user_id: user.id)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
+  end
+  describe "when user_id is not present" do
+     before { @micropost.user_id = nil }
+     it { should_not be_valid }
+   end
+
+   describe "with blank content" do
+     before { @micropost.content = " " }
+     it { should_not be_valid }
+   end
+
+   describe "with content that is too long" do
+     before { @micropost.content = "a" * 141 }
+     it { should_not be_valid }
+   end
 end
