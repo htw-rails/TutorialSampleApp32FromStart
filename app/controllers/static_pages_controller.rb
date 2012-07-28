88d1d9ac537a2334a0cbce854df5f160cdd9424a
B Kleinen <kleinen@infrastructure.de>
2012-07-28 11:26:56 +0200

    10.3.3 A proto-feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if signed_in?
+    if signed_in?
+      @micropost = current_user.microposts.build 
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
+  
   def help
   end
+  
   def about
   end
+  
   def contact
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,7 +32,21 @@ describe "Static pages" do
      click_link "sample app"
      page.should have_selector 'h1', text: 'Sample App'
     end
-   
+    describe "for signed-in users" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+          FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+          sign_in user
+          visit root_path
+        end
+
+        it "should render the user's feed" do
+          user.feed.each do |item|
+            page.should have_selector("li##{item.id}", text: item.content)
+          end
+        end
+      end
   end
 
   describe "Help page" do
