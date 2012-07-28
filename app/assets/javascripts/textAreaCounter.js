662e4976729c5a67f1918f74ede5f9a91bd08349
B Kleinen <kleinen@infrastructure.de>
2012-07-28 17:25:44 +0200

    10.5.7 - textarea counter working

--- app/assets/javascripts/textAreaCounter.js
+++ app/assets/javascripts/textAreaCounter.js
@@ -1,31 +1,14 @@
 	
 		var info;
 		$(document).ready(function(){
-			var options = {
-				'maxCharacterSize': -2,
-				'originalStyle': 'originalTextareaInfo',
-				'warningStyle' : 'warningTextareaInfo',
-				'warningNumber': 40
-			};
-			$('#testTextarea').textareaCount(options);
-
+			
 			var options2 = {
-					'maxCharacterSize': 200,
+					'maxCharacterSize': 140,
 					'originalStyle': 'originalTextareaInfo',
 					'warningStyle' : 'warningTextareaInfo',
 					'warningNumber': 40,
-					'displayFormat' : '#input/#max | #words words'
+					'displayFormat' : '#input/#max characters'
 			};
-			$('#testTextarea2').textareaCount(options2);
+			$('#micropostTextArea').textareaCount(options2);
 
-			var options3 = {
-					'maxCharacterSize': 200,
-					'originalStyle': 'originalTextareaInfo',
-					'warningStyle' : 'warningTextareaInfo',
-					'warningNumber': 40,
-					'displayFormat' : '#left Characters Left / #max'
-			};
-			$('#testTextarea3').textareaCount(options3, function(data){
-				$('#showData').html(data.input + " characters input. <br />" + data.left + " characters left. <br />" + data.max + " max characters. <br />" + data.words + " words input.");
-			});
 		});
